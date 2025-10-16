58c1ab6cdfb63ab97c35ac1bab0eb4ab187b3194
soye <stardust6653@gmail.com>
2025-10-16 15:44:45 +0900

    feat : 레이아웃 구성 및 SCSS 설치
    - 그 외 필요없는 보일러플레이트 파일 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/layout/Header";
+import Sidebar from "@/components/layout/Sidebar";
+import styles from "./layout.module.scss";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +28,11 @@ export default function RootLayout({
   return (
     <html lang="ko-Kr">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <Header />
+        <main className={styles.layoutMain}>
+          <Sidebar />
+          {children}
+        </main>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import styles from "./page.module.css";
+import Logo from "@/components/svg/Logo";
+import styles from "./home.module.scss";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-     
-    </div>
-  );
+  return <div className={styles.home}>content</div>;
 }
