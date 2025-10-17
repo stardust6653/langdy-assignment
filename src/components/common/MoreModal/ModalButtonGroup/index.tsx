0a7e6194ee3a7a3750840e2d94718d7d80379594
soye <stardust6653@gmail.com>
2025-10-17 16:12:36 +0900

    feat : 채널 공유 기능 추가

--- src/components/common/MoreModal/ModalButtonGroup/index.tsx
+++ src/components/common/MoreModal/ModalButtonGroup/index.tsx
@@ -1,13 +1,16 @@
 import { MdOutlineIosShare } from "react-icons/md";
 import { MdOutlineFlag } from "react-icons/md";
 import styles from "../MoreModal.module.scss";
+import { copyUrl } from "@/utils/copyUrl";
 
 const ModalButtonGroup = () => {
   return (
     <div className={styles.buttonGroup}>
       <button>
         <MdOutlineIosShare size={18} className={styles.buttonIcon} />
-        <span className={styles.content}>채널 공유</span>
+        <span className={styles.content} onClick={copyUrl}>
+          채널 공유
+        </span>
       </button>
       <button>
         <MdOutlineFlag size={18} className={styles.buttonIcon} />
