04002a573bb73adeb15de07257e88329f6f005db
soye <stardust6653@gmail.com>
2025-10-17 13:08:59 +0900

    feat : 비디오 필터 기능 추가

--- src/components/page/videos/VideoList/index.tsx
+++ src/components/page/videos/VideoList/index.tsx
@@ -1,8 +1,7 @@
 import { useFetch } from "@/hooks/useFetch";
 import { FilterType, VideoData } from "@/types/videos";
 import styles from "./VideoList.module.scss";
-import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import VideoItem from "../VideoItem";
 import VideoFilter from "../VideoFilter";
 
@@ -11,6 +10,36 @@ const VideoList = () => {
   const [currentFilter, setCurrentFilter] = useState<FilterType>("최신순");
   const [filteredVideos, setFilteredVideos] = useState<VideoData[]>(data || []);
 
+  useEffect(() => {
+    if (data) {
+      if (currentFilter === "최신순") {
+        const sortedByLatest = [...data].sort((a, b) => {
+          return (
+            new Date(b.publishedAt).getTime() -
+            new Date(a.publishedAt).getTime()
+          );
+        });
+        setFilteredVideos(sortedByLatest);
+      } else if (currentFilter === "인기순") {
+        const sortedByPopularity = [...data].sort((a, b) => {
+          return (
+            parseInt(b.viewCount.replace(/,/g, "")) -
+            parseInt(a.viewCount.replace(/,/g, ""))
+          );
+        });
+        setFilteredVideos(sortedByPopularity);
+      } else if (currentFilter === "날짜순") {
+        const sortedByOldest = [...data].sort((a, b) => {
+          return (
+            new Date(a.publishedAt).getTime() -
+            new Date(b.publishedAt).getTime()
+          );
+        });
+        setFilteredVideos(sortedByOldest);
+      }
+    }
+  }, [data, currentFilter]);
+
   return (
     <div className={styles.videoListContainer}>
       <VideoFilter
@@ -18,7 +47,7 @@ const VideoList = () => {
         setCurrentFilter={setCurrentFilter}
       />
       <div className={styles.videoList}>
-        {data?.map((video) => {
+        {filteredVideos?.map((video) => {
           return <VideoItem key={video.id} video={video} />;
         })}
       </div>
